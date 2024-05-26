bba49edbfb5ef547b51bde79d1ff4d35cca84c00
test <Dineshwonks@Gmail.com>
2024-05-26 15:07:35 +0530

    geo Location

---
+++ Geo Location/app.js
@@ -0,0 +1,10 @@
+const result = document.querySelector(".location");
+const btn = document.querySelector("#findMe");
+
+const showPosition = (pos)  => {result.innerHTML = `Latitude: ${pos.coords.latitude} <br> Longitude: ${pos.coords.longitude}`};
+
+const getUserLocation = () => {
+    navigator.geolocation.getCurrentPosition(showPosition);
+} 
+
+btn.addEventListener('click',getUserLocation);
